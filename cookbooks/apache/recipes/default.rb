1091aef83edbd2b04513b950006801bef6de53f4
root <root@alimujju213.mylabserver.com>
2018-01-06 05:04:58 +0000

    nodes attributes

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -3,9 +3,15 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+if node['platform_family'] == "rhel"
+   package  = "httpd"
+elsif node['platform_family'] == "debian"
+       package = "apache"
+end
+
 
 package "apache" do
-   package_name "httpd"
+   package_name package
    action :install
  end
   
